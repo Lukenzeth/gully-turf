be3e28fed2e04dae3f8fc9f9652e10c984d0ea1a
Luke Stancliffe-White <luke.stancliffe.white@gmail.com>
2026-03-17 16:29:15 +1300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GullyTurfBooking from './GullyTurfBooking'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GullyTurfBooking />
+  </React.StrictMode>
+)
